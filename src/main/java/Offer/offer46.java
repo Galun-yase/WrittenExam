7dae940a3d5fd63c0bcf93a8a95dbc31d7c9bf61
rcn <534897552rqc@gmail.com>
2020-08-08 08:50:12 +0800

    完成1道，8月6日

---
+++ src/main/java/Offer/offer46.java
@@ -0,0 +1,60 @@
+package Offer;
+
+//循环链表
+public class offer46 {
+    class Node{
+        int val;
+        Node next;
+        Node(int val){
+            this.val=val;
+            next=null;
+        }
+    }
+
+    private Node deleteNode(Node node){
+        Node p =node;
+        while (p.next.val!=node.val){
+            p=p.next;
+        }
+        p.next=node.next;
+        node.next=null;
+        return p.next;
+    }
+
+    public int LastRemaining_Solution(int n, int m) {
+        if (n<=0 || m<= 0)return -1;
+
+        Node head=new Node(0);
+        Node p = head;
+
+        for (int i = 1; i < n; i++) {
+
+            Node node=new Node(i);
+            p.next=node;
+            p=node;
+
+        }
+        p.next=head;
+
+        Node pp = head;
+        while (pp.next.next.val!=pp.val){
+            for (int i = 1; i < m; i++) {
+                pp=pp.next;
+            }
+            pp = deleteNode(pp);
+        }
+        if (m%2==0){
+            return pp.val;
+        }else {
+            return pp.next.val;
+        }
+    }
+
+    public static void main(String[] args) {
+        new offer46().LastRemaining_Solution(5,3);
+    }
+
+
+
+
+}
