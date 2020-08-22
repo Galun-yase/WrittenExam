9678ecff00ddcd8a11bf35b861a63aff1e5c5b3f
renqingcheng <rcn@servyou.com.cn>
2020-08-22 21:11:20 +0800

    2020年8月22日 两道LeetCode 难顶

--- src/main/java/LeetCode/s3.java
+++ src/main/java/LeetCode/s3.java
@@ -3,33 +3,36 @@ package LeetCode;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author 任青成
  * @date 2020/8/22 19:14
  */
+//巧妙利用边界 滑动窗口 最长子串
 public class s3 {
 
     public int lengthOfLongestSubstring(String s) {
-        if (s==null)return 0;
-        if (s.length()<=1)return s.length();
-        char[] chars = s.toCharArray();
-        int[] res = new int[s.length()];
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> occ = new HashSet<Character>();
+        int n = s.length();
+        int rk = -1;
+        int ans = 0;
 
-        for (int i = 0; i < chars.length; i++) {
-            HashSet<Character> set = new HashSet<>();
-            set.add(chars[i]);
-            res[i]=1;
-            for (int j = i+1; j < chars.length; j++) {
-                if (set.contains(chars[j]))break;
-                res[i]+=1;
-                set.add(chars[j]);
+        for (int i = 0; i < n; i++) {
+
+            if (i!=0){
+                occ.remove(s.charAt(i-1));
             }
-
+            while (rk+1<n && !occ.contains(s.charAt(rk+1))){
+                occ.add(s.charAt(rk+1));
+                rk++;
+            }
+            ans = Math.max(ans,rk-i+1);
 
         }
-        Arrays.sort(res);
-        return res[s.length()-1];
+        return ans;
+
     }
 
     public static void main(String[] args) {
