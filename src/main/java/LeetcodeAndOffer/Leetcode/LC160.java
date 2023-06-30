243f249bc8e6b9786c2e8cafcdd38a2c31be7d06
renqingcheng <renchen_zrr@foxmail.com>
2023-06-30 23:26:40 +0800

    2023年06月29日 吴师兄算法训练营第五天，两道链表题目，比较简单经典，优化版本

--- src/main/java/LeetcodeAndOffer/Leetcode/LC160.java
+++ src/main/java/LeetcodeAndOffer/Leetcode/LC160.java
@@ -3,36 +3,48 @@ package LeetcodeAndOffer.Leetcode;
 
 public class LC160 {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // 1、优化版本，数学思想，公倍数
+        if (headA == null || headB == null) return null;
 
-        ListNode A = headA;
-        ListNode B = headB;
+        ListNode pointA = headA;
+        ListNode pointB = headB;
 
-        int headA_length = 0;
-        int headB_length = 0;
-        while (A != null) {
-            A = A.next;
-            headA_length++;
+        while (pointA != pointB) {
+            pointA = pointA == null ? headB : pointA.next;
+            pointB = pointB == null ? headA : pointB.next;
         }
-        while (B != null) {
-            B = B.next;
-            headB_length++;
-        }
+        return pointA;
 
-        int dis = Math.abs(headA_length - headB_length);
-        ListNode fast = headA_length > headB_length ? headA : headB;
-        ListNode slow = headA_length <= headB_length ? headA : headB;
-
-        while (dis > 0) {
-            fast = fast.next;
-            dis--;
-        }
-
-        while (fast != null) {
-            if (fast == slow) return fast;
-            fast = fast.next;
-            slow = slow.next;
-        }
-        return null;
+        // 2、计算长度
+//        ListNode A = headA;
+//        ListNode B = headB;
+//
+//        int headA_length = 0;
+//        int headB_length = 0;
+//        while (A != null) {
+//            A = A.next;
+//            headA_length++;
+//        }
+//        while (B != null) {
+//            B = B.next;
+//            headB_length++;
+//        }
+//
+//        int dis = Math.abs(headA_length - headB_length);
+//        ListNode fast = headA_length > headB_length ? headA : headB;
+//        ListNode slow = headA_length <= headB_length ? headA : headB;
+//
+//        while (dis > 0) {
+//            fast = fast.next;
+//            dis--;
+//        }
+//
+//        while (fast != null) {
+//            if (fast == slow) return fast;
+//            fast = fast.next;
+//            slow = slow.next;
+//        }
+//        return null;
 
     }
 
