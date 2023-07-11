66ac44f7b65a9372c057bd013b88c1e015be8606
renqingcheng <renchen_zrr@foxmail.com>
2023-07-11 23:24:29 +0800

    2023年07月11日 吴师兄算法训练营第十六天，岛屿问题 深度搜索DFS 补充并查集解法

--- src/main/java/LeetcodeAndOffer/Leetcode/LC200.java
+++ src/main/java/LeetcodeAndOffer/Leetcode/LC200.java
@@ -31,18 +31,18 @@ public class LC200 {
 
         // 如下需要考虑四个方向
         // char[][] ints2 = new char[][]{{'1', '1', '1'}, {'0', '1', '0'}, {'1', '1', '1'}};
-        int[] dx = new int[]{-1,1,0,0};
-        int[] dy = new int[]{0,0,-1,1};
+        int[] dx = new int[]{-1, 1, 0, 0};
+        int[] dy = new int[]{0, 0, -1, 1};
 
         for (int i = 0; i < 4; i++) {
             int newX = dx[i] + x;
             int newY = dy[i] + y;
 
-            if (newX<0 || newY<0 || newX>=mark.length|| newY>=mark[0].length){
+            if (newX < 0 || newY < 0 || newX >= mark.length || newY >= mark[0].length) {
                 continue;
             }
 
-            if (mark[newX][newY] ==0 && gird[newX][newY] =='1'){
+            if (mark[newX][newY] == 0 && gird[newX][newY] == '1') {
                 DFS(mark, newX, newY, gird);
             }
         }
@@ -54,23 +54,95 @@ public class LC200 {
 
         // 如下需要考虑四个方向
         // char[][] ints2 = new char[][]{{'1', '1', '1'}, {'0', '1', '0'}, {'1', '1', '1'}};
-        int[] dx = new int[]{-1,1,0,0};
-        int[] dy = new int[]{0,0,-1,1};
+        int[] dx = new int[]{-1, 1, 0, 0};
+        int[] dy = new int[]{0, 0, -1, 1};
 
         for (int i = 0; i < 4; i++) {
             int newX = dx[i] + x;
             int newY = dy[i] + y;
 
-            if (newX<0 || newY<0 || newX>=gird.length|| newY>=gird[0].length){
+            if (newX < 0 || newY < 0 || newX >= gird.length || newY >= gird[0].length) {
                 continue;
             }
 
-            if (gird[newX][newY] =='1'){
+            if (gird[newX][newY] == '1') {
                 DFS(newX, newY, gird);
             }
         }
     }
 
+    // 补充并查集
+    public int numIslands_union_find(char[][] grid) {
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        UnionFind uf = new UnionFind(rows * cols);
+        int waters = 0;
+
+        int[] dx = new int[]{1, 0};
+        int[] dy = new int[]{0, 1};
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '0') {
+                    waters++;
+                } else {
+                    for (int k = 0; k < 2; k++) {
+                        int newI = dx[k] + i;
+                        int newJ = dy[k] + j;
+
+                        if (newI < rows && newJ < cols && grid[newI][newJ] == '1') {
+                            uf.union(getIndex(cols, i, j), getIndex(cols, newI, newJ));
+                        }
+                    }
+                }
+            }
+        }
+
+        return uf.getCount() - waters;
+
+    }
+
+    private int getIndex(int n, int i, int j) {
+        return n * i + j;
+
+    }
+
+    static class UnionFind {
+
+        int count;
+        int[] roots;
+
+        UnionFind(int n) {
+            count = n;
+            roots = new int[n];
+            for (int i = 0; i < n; i++) {
+                roots[i] = i;
+            }
+        }
+
+        public int find(int i) {
+            if (roots[i] == i) return i;
+            return find(roots[i]);
+        }
+
+        public void union(int p, int q) {
+            int pRoots = find(roots[p]);
+            int qRoots = find(roots[q]);
+
+            if (pRoots != qRoots) {
+                roots[pRoots] = qRoots;
+                count--;
+            }
+
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+    }
 
 
     public static void main(String[] args) {
