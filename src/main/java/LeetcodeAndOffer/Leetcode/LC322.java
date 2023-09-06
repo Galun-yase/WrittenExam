2a775a3b030fb408d44a71382fdc436a1c5fa2f5
renqingcheng <renchen_zrr@foxmail.com>
2023-09-06 23:21:21 +0800

    2023年09月06日 [算法训练营]第四十九天，动态规划，完全背包与01差别在于前者可以无限取，即取了当前元素之后还能取，所以为dp[i]而不是dp[i-1]

--- src/main/java/LeetcodeAndOffer/Leetcode/LC322.java
+++ src/main/java/LeetcodeAndOffer/Leetcode/LC322.java
@@ -23,4 +23,31 @@ public class LC322 {
         }
         return dp[amount];
     }
+
+    // 完全背包解法
+    public int coinChange_second(int[] coins, int amount) {
+
+        int[][] dp = new int[coins.length + 1][amount + 1];
+
+        for (int i = 0; i < coins.length + 1; i++) {
+            for (int j = 0; j < amount + 1; j++) {
+                dp[i][j] = amount + 1;
+            }
+        }
+
+        dp[0][0] = 0;
+
+        for (int i = 1; i < coins.length + 1; i++) {
+            for (int j = 0; j < amount + 1; j++) {
+
+                if (j < coins[i - 1]) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    // 完全背包，此处dp[i][j - coins[i - 1]] + 1不用dp[i-1]，可以拿多次
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - coins[i - 1]] + 1);
+                }
+            }
+        }
+        return dp[coins.length][amount] == amount + 1 ? -1 : dp[coins.length][amount];
+    }
 }
---
+++ src/main/java/LeetcodeAndOffer/Leetcode/LC518.java
@@ -0,0 +1,21 @@
+package LeetcodeAndOffer.Leetcode;
+
+public class LC518 {
+    public int change(int amount, int[] coins) {
+
+        int[][] dp = new int[coins.length + 1][amount + 1];
+        dp[0][0] = 1;
+
+        for (int i = 1; i < coins.length + 1; i++) {
+            for (int j = 0; j < amount + 1; j++) {
+
+                if (j < coins[i - 1]) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                }
+            }
+        }
+        return dp[coins.length][amount];
+    }
+}
