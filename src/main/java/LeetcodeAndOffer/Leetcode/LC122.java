6b687fcddbfefb80beb78475ed10a99079fb4804
renqingcheng <renchen_zrr@foxmail.com>
2024-01-21 16:33:40 +0800

    2024年01月21日 [算法训练营二刷]第四十五天，买卖股票问题，看是否无限次操作，是否是二维数组还是三维数组

--- src/main/java/LeetcodeAndOffer/Leetcode/LC122.java
+++ src/main/java/LeetcodeAndOffer/Leetcode/LC122.java
@@ -42,4 +42,18 @@ public class LC122 {
         return res;
     }
 
+    public int maxProfit_4(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[prices.length - 1][0];
+    }
+
 }
--- src/main/java/LeetcodeAndOffer/Leetcode/LC123.java
+++ src/main/java/LeetcodeAndOffer/Leetcode/LC123.java
@@ -23,4 +23,28 @@ public class LC123 {
         }
         return dp[length - 1][k][0];
     }
+
+    public int maxProfit_2(int[] prices) {
+        int k = 2;
+        if (prices == null || prices.length == 0 || k == 0) return 0;
+
+        // 第i天交易k次 持有不持有状态
+        int[][][] dp = new int[prices.length][k + 1][2];
+
+        for (int i = 1; i <= k; i++) {
+            dp[0][i][0] = 0;
+            dp[0][i][1] = -prices[0];
+        }
+
+        for (int i = 1; i < prices.length; i++) {
+            for (int j = 1; j <= k; j++) {
+
+                dp[i][j][0] = Math.max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
+                dp[i][j][1] = Math.max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i]);
+
+            }
+        }
+
+        return dp[prices.length - 1][k][0];
+    }
 }
